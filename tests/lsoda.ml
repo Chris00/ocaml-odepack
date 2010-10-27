5dd3d3f387eb447c84b0a2434cbac1e80e647ef2
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-10-27 21:57:55 +0200

    Test: fixed typos and presision.

--- tests/lsoda.ml
+++ tests/lsoda.ml
@@ -8,14 +8,14 @@ let vec a = Array1.of_array float64 fortran_layout a
 let f t (y: vec) (y': vec) =
   y'.{1} <- -0.04 *. y.{1} +. 1e4 *. y.{2} *. y.{3};
   y'.{3} <- 3e7 *. y.{2} *. y.{2};
-  y'.{2} <- -. y'.{2} -. y'.{3}
+  y'.{2} <- -. y'.{1} -. y'.{3}
 
 let () =
   let y = vec [| 1.; 0.; 0. |] in
   let atol_vec = vec [| 1e-6; 1e-10; 1e-6 |] in
-  let ode = Odepack.lsoda f 0. y 0. ~atol_vec in
-  for i = 1 to 11 do
+  let ode = Odepack.lsoda f 0. y 0. ~atol_vec ~rtol:1e-4 in
+  for i = 0 to 11 do
     let t = 0.4 *. 10.**(float i) in
     Odepack.advance ode t;
-    printf "At t = %e  Y = %e %e %e\n" t y.{1} y.{2} y.{3}
+    printf "At t = %e  Y = %e %e %e\n" t y.{1} y.{2} y.{3};
   done

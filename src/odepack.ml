f64f8b8d14fad128ab1ff9abf5830f2f2ce67fd3
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-10-27 21:57:15 +0200

    More general representation of Odepack.t to work for other subroutines.

--- src/odepack.ml
+++ src/odepack.ml
@@ -42,16 +42,14 @@ type 'a t = {
   f: 'a vec_field;
   mutable t: float;
   y: 'a vec;
-  itol: int;
-  rtol: float;
-  atol: 'a vec;
   mutable state: int;
   rwork: fortran_layout vec;
   iwork: fortran_layout int_vec;
-  ydot: 'a vec; (* The bigarray created to pass ydot to [f]. *)
+  advance: float -> unit
 }
 
-let get ode = ode.y
+let t ode = ode.t
+let vec ode = ode.y
 let hu ode = ode.rwork.{11}
 let hcur ode = ode.rwork.{12}
 let tcur ode = ode.rwork.{13}
@@ -63,16 +61,17 @@ let nje ode = ode.iwork.{13}
 let nqu ode = ode.iwork.{14}
 let nqcur ode = ode.iwork.{15}
 let imxer ode = ode.iwork.{16} (* FIXME: fortran/C layout *)
+let advance ode = ode.advance
 
-let lsoda ?(rtol=1e-6) ?(atol=1e-6) ?atol_vec f t y tout =
-  let neq = Array1.dim y in
+let lsoda ?(rtol=1e-6) ?(atol=1e-6) ?atol_vec f t0 y0 tout =
+  let neq = Array1.dim y0 in
   let itol, atol = match atol_vec with
     | Some v ->
       if Array1.dim v <> neq then
         invalid_arg "Odepack.lsoda: dim atol_vec <> size ODE system";
       2, v
     | None ->
-      let v = Array1.create float64 (Array1.layout y) 1 in
+      let v = Array1.create float64 (Array1.layout y0) 1 in
       v.{1} <- atol;
       1, v in
   (* For now, JT = 2 *)
@@ -81,17 +80,21 @@ let lsoda ?(rtol=1e-6) ?(atol=1e-6) ?atol_vec f t y tout =
   let dim_rwork = if lrn > lrs then lrn else lrs in
   let rwork = Array1.create float64 fortran_layout dim_rwork
   and iwork = Array1.create int fortran_layout (20 + neq) in
-  let ydot = set_layout (Array1.sub rwork 1 neq) (Array1.layout y) in
-  let state = lsoda_fortran f y t tout ~itol ~rtol ~atol TOUT ~state:1
+  (* Create a bigarray, proxy for rwork, that will encapsulate the
+     array of devivatives for OCaml. *)
+  let ydot = set_layout (Array1.sub rwork 1 neq) (Array1.layout y0) in
+  let state = lsoda_fortran f y0 t0 tout ~itol ~rtol ~atol TOUT ~state:1
     ~rwork ~iwork ydot in
   if state = -3 then invalid_arg "Odepack.lsoda (see written message)";
-  { f = f;  t = t; y = y;  itol = itol; rtol = rtol; atol = atol;
-    state = state;  rwork = rwork;  iwork = iwork;  ydot = ydot }
 
-let advance o t =
-  let state = lsoda_fortran o.f o.y o.t t
-    ~itol:o.itol ~rtol:o.rtol ~atol:o.atol TOUT ~state:o.state
-    ~rwork:o.rwork ~iwork:o.iwork o.ydot in
-  if state = -3 then invalid_arg "Odepack.advance (see written message)";
-  o.t <- t;
-  o.state <- state
+  let rec advance t =
+    let state = lsoda_fortran f y0 t0 t ~itol ~rtol ~atol TOUT ~state:ode.state
+      ~rwork ~iwork ydot in
+    if state = -3 then invalid_arg "Odepack.advance (see written message)";
+    ode.t <- t;
+    ode.state <- state
+  and ode = { f = f;  t = tout; y = y0;
+              state = state;  rwork = rwork;  iwork = iwork;
+              advance = advance } in
+  ode
+

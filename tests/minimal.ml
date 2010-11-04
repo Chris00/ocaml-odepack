328d7d6b5065c166d77d2f18b14882bc9c6e2a90
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-11-04 22:13:39 +0100

    The minimal test computes sin and save the result into a file.

--- tests/minimal.ml
+++ tests/minimal.ml
@@ -2,9 +2,22 @@
 open Printf
 open Bigarray
 
+let fname = Filename.temp_file "odepack" ".dat"
+
 let () =
-  let y = Array1.of_array float64 fortran_layout [| 0. |] in
-  let f _ _ y' = y'.{1} <- 1. in
-  let t = 1. in
-  ignore(Odepack.lsoda f y 0. t);
-  printf "y(t=%g) = %g\n" t y.{1}
+  let f _ y y' =
+    y'.{1} <- y.{2};
+    y'.{2} <- -. y.{1} in
+  let y = Array1.of_array float64 fortran_layout [| 0.; 1. |] in
+  let t = 10. in
+  let n = 100 in
+  let dt = t /. float(n-1) in
+  let ode = Odepack.lsoda f y 0. 0. in
+  let fh = open_out fname in
+  for i = 0 to n - 1 do
+    let t = float i *. dt in
+    let y = Odepack.sol ode t in
+    fprintf fh "%f %f %f\n" t y.{1} y.{2}
+  done;
+  close_out fh;
+  printf "Wrote %S.\n" fname

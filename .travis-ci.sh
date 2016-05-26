f217b4adc33cb5dab7fcbaf47afa58c9a6e8fcff
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2016-05-26 10:39:24 +0200

    Add Travis continuous integration

---
+++ .travis-ci.sh
@@ -0,0 +1,26 @@
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    brew install ocaml opam
+fi
+
+OPAM_PKGS="oasis base-bytes"
+
+export OPAMYES=1
+
+if [ -f "$HOME/.opam/config" ]; then
+    opam update
+    opam upgrade
+else
+    opam init
+fi
+
+if [ -n "${OPAM_SWITCH}" ]; then
+    opam switch ${OPAM_SWITCH}
+fi
+eval `opam config env`
+
+opam install $OPAM_PKGS
+
+export OCAMLRUNPARAM=b
+make
+make test
